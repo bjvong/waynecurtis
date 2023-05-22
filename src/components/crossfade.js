5749f32b696b9ff1d66f5e8e9ae3d0240852eeae
Beau VonGunten <bjvong@gmail.com>
2023-05-22 18:26:55 -0400

    what is broken

--- src/components/crossfade.js
+++ src/components/crossfade.js
@@ -2,216 +2,217 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function FadeBanner() {
   return (
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/1.JPG"
-          
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Porch Windows</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/2.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Replaced old storms with vinyl double hung.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/stuff 170.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Nutwood's Incredible Facelift</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/stuff 168.jpg"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Nutwood's Incredible Facelift</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/IMG_0111.JPG"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Relocate Laundry</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/IMG_0141.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Move laundry room to basement and add water supply</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/IMG_1829.JPG"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Drain doesn't exist</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/IMG_1848.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Installed new floor drain</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/IMG_1845.JPG"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Snatch Patch</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/IMG_1850.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>New Snatch Patch</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/sherwood, aprtment bldg,chandler 017.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Landscape</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/DSC_0005.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Tree removal and Landscape</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/apartment 009.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Kitchen Trashed</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/DSC_0016.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Kitchen Updated</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/9.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Kitchen Update</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/DSC_0016.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Kitchen Update</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/IMG_0161.JPG"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Deck Installation</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/IMG_0222.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>8x8 Deck Added</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-    </Carousel>
+    <></>
+    // <Carousel fade>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/1.JPG"
+
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Porch Windows</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/2.JPG"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>Replaced old storms with vinyl double hung.</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/stuff 170.jpg"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Nutwood's Incredible Facelift</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/stuff 168.jpg"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>Nutwood's Incredible Facelift</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/IMG_0111.JPG"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Relocate Laundry</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/IMG_0141.JPG"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>Move laundry room to basement and add water supply</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/IMG_1829.JPG"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Drain doesn't exist</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/IMG_1848.JPG"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>Installed new floor drain</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/IMG_1845.JPG"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Snatch Patch</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/IMG_1850.JPG"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>New Snatch Patch</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/sherwood, aprtment bldg,chandler 017.jpg"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Landscape</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/DSC_0005.JPG"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>Tree removal and Landscape</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/apartment 009.jpg"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Kitchen Trashed</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/DSC_0016.JPG"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>Kitchen Updated</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/9.jpg"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Kitchen Update</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/DSC_0016.JPG"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>Kitchen Update</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/before/IMG_0161.JPG"
+    //       alt="First slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>Before</h3>
+    //       <p>Deck Installation</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <img
+    //       className="d-block w-100"
+    //       src="/img/after/IMG_0222.JPG"
+    //       alt="Second slide"
+    //     />
+    //     <Carousel.Caption>
+    //       <h3>After</h3>
+    //       <p>8x8 Deck Added</p>
+    //     </Carousel.Caption>
+    //   </Carousel.Item>
+    //   {/* /////////////////////////////////////////////////// */}
+    // </Carousel>
   );
 }
 
