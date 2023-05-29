325e5472c70d64e513d12199f9d2038d832ba636
Beau VonGunten <bjvong@gmail.com>
2023-05-29 14:40:21 -0400

    redo carousel

--- src/components/crossfade.js
+++ src/components/crossfade.js
@@ -2,77 +2,60 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function FadeBanner() {
   return (
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/1.JPG"
+        <Carousel fade>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/img/before/1.JPG"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/img/after/2.JPG"
+              alt="Second slide"
+            />
+    
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/img/before/stuff 170.jpg"
+              alt="Third slide"
+            />
+    
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/img/after/stuff 168.jpg"
+              alt="Third slide"
+            />
+    
+            <Carousel.Caption>
+              <h3>Fourth slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
 
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Porch Windows</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/2.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Replaced old storms with vinyl double hung.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/stuff 170.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Nutwood's Incredible Facelift</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/stuff 168.jpg"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Nutwood's Incredible Facelift</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      {/* /////////////////////////////////////////////////// */}
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/before/IMG_0111.JPG"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Before</h3>
-          <p>Relocate Laundry</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/img/after/IMG_0141.JPG"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>After</h3>
-          <p>Move laundry room to basement and add water supply</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
   );
 }
 
