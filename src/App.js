5f6f35de013f05fe85236099365acedc15be848e
Beau VonGunten <bjvong@gmail.com>
2023-05-29 15:53:47 -0400

    trying something out

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import FadeBanner from "./components/crossfade";
 import FooterNav from "./components/footer";
 import BasicNav from "./components/navbar";
+import React, { Component } from 'react';
 
-// var ws = new WebSocket('wss://wayne-curtis.herokuapp.com/');
+class App extends Component {
+  state = {
+      data: null
+    };
+    componentDidMount() {
+      this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    }
 
-function App() {
+    callBackendAPI = async () => {
+      const response = await fetch('/express_backend');
+      const body = await response.json();
+  
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      return body;
+    };
+
+// function App() {
+render(){
   return (<>
 <BasicNav/>
 <FadeBanner/>
 <FooterNav/>
 </>
   );
+}
 }
 
 export default App;
--- src/components/footer.js
+++ src/components/footer.js
@@ -10,35 +10,7 @@ import {
 
 function FooterNav() {
   return (
-    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
-    {/* <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-      <div className='me-5 d-none d-lg-block'>
-        <span>Get connected with us on social networks:</span>
-      </div>
-
-      <div>
-        <a href='' className='me-4 text-reset'>
-          <MDBIcon color='secondary' fab icon='facebook-f' />
-        </a>
-        <a href='' className='me-4 text-reset'>
-          <MDBIcon color='secondary' fab icon='twitter' />
-        </a>
-        <a href='' className='me-4 text-reset'>
-          <MDBIcon color='secondary' fab icon='google' />
-        </a>
-        <a href='' className='me-4 text-reset'>
-          <MDBIcon color='secondary' fab icon='instagram' />
-        </a>
-        <a href='' className='me-4 text-reset'>
-          <MDBIcon color='secondary' fab icon='linkedin' />
-        </a>
-        <a href='' className='me-4 text-reset'>
-          <MDBIcon color='secondary' fab icon='github' />
-        </a>
-      </div>
-    </section> */}
-
-    <section className=''>
+    <MDBFooter bgColor='white' className='text-center text-lg-start text-muted'>
       <MDBContainer className='text-center text-md-start mt-5'>
         <MDBRow className='mt-3'>
           <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
@@ -119,12 +91,12 @@ function FooterNav() {
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-    </section>
+
 
     <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-      © 2021 Copyright:
+      ©&nbsp;
       <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-        MDBootstrap.com
+        Wayne Curtis, LLC
       </a>
     </div>
   </MDBFooter>
